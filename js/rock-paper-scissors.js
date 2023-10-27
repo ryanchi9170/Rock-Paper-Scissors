8103ab27e96f57e46bdef5cecaa61096d0724d01
Ryan Chi <ryanchi9170@gmail.com>
2023-10-27 04:08:10 -0700

    Created getPlayerChoice and game function

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -3,6 +3,20 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
+function getPlayerChoice() {
+  let choice = prompt("Choose rock, paper, or scissors");
+    while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+      choice = prompt("Choose rock, paper, or scissors");
+      if (choice === "rock" || choice === "paper" || choice === "scissors") {
+        break;
+      }
+      else {
+        continue;
+      }
+    }
+  return choice.toLowerCase();
+}
+
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     return "Draw!";
@@ -12,5 +26,33 @@ function playRound(playerSelection, computerSelection) {
   }
   else {
     return "You Lose!";
+  }
+}
+
+function game() {
+  let playerScore = 0,
+      computerScore = 0;
+  while (playerScore < 5 && computerScore < 5) {
+    let computerChoice = getComputerChoice();
+    let playerChoice = getPlayerChoice();
+    let result = playRound(playerChoice, computerChoice);
+    if (result === "Draw!") {
+      console.log("Draw!");
+    }
+    else if (result === "You Win!") {
+      console.log("You Win!")
+      playerScore++;
+    }
+    else {
+      console.log("You Lose!")
+      computerScore++;
+    }
+    console.log(`Player Score: ${playerScore} | Computer Score: ${computerScore}`);
+  }
+  if (playerScore === 5) {
+    console.log("Player Wins!");
+  }
+  else {
+    console.log("Computer Wins!");
   }
 }
