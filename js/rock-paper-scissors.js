8f8bca16ed28da548607f2a2bb064473fd91c6a5
Ryan Chi <ryanchi9170@gmail.com>
2023-10-27 03:15:18 -0700

    Created playRound function

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -2,3 +2,15 @@ function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
 }
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "Draw!";
+  }
+  else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || playerSelection === "scissors" && computerSelection === paper) {
+    return "You Win!";
+  }
+  else {
+    return "You Lose!";
+  }
+}
